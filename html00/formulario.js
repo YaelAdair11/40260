1f881b9e1e8ec8809327862f677924bd485cc639
YaelAdair11 <yaelgtz11@gmail.com>
2025-05-05 12:00:09 -0600

    07 formulario

---
+++ html00/formulario.js
@@ -0,0 +1,13 @@
+console.log("inicio")
+
+const boton = document.getElementById("enviar")
+
+boton.addEventListener("click", 
+    ()=> {
+        console.log("los parámetros")
+        let user=document.getElementById("user")
+        let password=document.getElementById("password")
+        console.log(password.value)
+        console.log(user.value)
+    }
+)
